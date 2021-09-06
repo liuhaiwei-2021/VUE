48a9ca8e77d0e37a2d2a3b3dcaef560cb38d1573
unknown <liuhaiwei2016@gmail.com>
2021-09-06 15:25:30 +0200

    events

---
+++ 29.events/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <div class="container">
+      <div class="row">
+        <div class="col-6 border py-5 m-5">
+          <SignIn />
+
+        </div>
+
+      </div>
+ 
+
+    </div>
+    
+  </div>
+</template>
+
+<script>
+ import Navbar from './components/Navbar.vue'
+ import SignIn from './components/SignIn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    SignIn
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
