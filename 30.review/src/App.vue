44252ff794ce8500aa68fffc0d632faa62a58687
Haiwei Liu <haiweiliu@Haiweis-MacBook-Air.local>
2021-10-18 10:43:43 +0200

    add

---
+++ 30.review/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <nav-bar />
+    <hero />
+
+  </div>
+</template>
+
+<script>
+
+import navbar from './components/navbar.vue'
+import Hero from './components/Hero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navBar:navbar,
+    hero:Hero
+    
+  }
+}
+</script>
+
+<style>
+</style>
