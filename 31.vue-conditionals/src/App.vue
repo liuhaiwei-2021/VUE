ef11fd96ffd89ad186df020f62f9afc6bcb91a14
Haiwei Liu <haiweiliu@Haiweis-MacBook-Air.local>
2021-10-22 12:41:10 +0200

    conditionals

---
+++ 31.vue-conditionals/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app container">
+    <Navbar  title ='Vue conditionals' :signedIn='checkRole'/>
+
+  </div>
+</template>
+
+<script>
+
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+         signedIn: true,
+         isAdmin:false
+    }
+  },
+  components: {
+    Navbar
+  },
+  computed:{
+    checkRole(){
+      if(this.isAdmin&&this.signedIn)
+        return true
+      else
+        return false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 31.vue-conditionals/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar navbar-light navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      {{title}}
+    </a>
+    
+     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div v-if="signedIn" class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+
+    <div v-else>
+      <button class="btn btn-light">  
+        Sign In
+      </button>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  props:{
+    title:String,
+    signedIn:Boolean
+  }
+
+}
+</script>
+
+<style>
+
+</style>
