42b03904cc759cf216d411eca243353abc416325
Haiwei Liu <haiweiliu@Haiweis-MacBook-Air.local>
2021-11-19 14:36:34 +0100

    dynmic component

---
+++ 33.dynamic-component/src/components/User.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mt-4 border">
+      <button class="btn btn-primary">  sign in </button>
+      <button class="btn btn-outline-primary"> sign up </button>
+      <SignIn />
+      <SignUp />
+
+
+  </div>
+</template>
+
+
+<script>
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+
+export default {
+    components:{
+        SignIn,
+        SignUp
+    }
+
+}
+</script>
+
+<style>
+
+</style>
